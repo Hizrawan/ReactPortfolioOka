2560c3023e8aee0dbb69c5ffa96574e75ce795db
Hizrawan Dwi Oka <hizrawandwioka@gmail.com>
2024-07-09 15:40:33 +0800

    config vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+  },
   base: './',
   plugins: [react()],
   build: {
