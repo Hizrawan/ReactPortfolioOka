678ceded85860cb8ad7afdfa2f206166f508e9d9
Hizrawan Dwi Oka <hizrawandwioka@gmail.com>
2024-07-10 15:13:32 +0800

    Code Review

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.jsx?$/,
-  },
-  base: './',
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
+    esbuild: {
+        loader: 'jsx',
+        include: /src\/.*\.jsx?$/,
     },
-    outDir: 'build',
-  },
-  
+    base: './',
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name].js',
+                assetFileNames: 'assets/[name].[ext]'
+            }
+        },
+        outDir: 'build',
+    },
+
 })
